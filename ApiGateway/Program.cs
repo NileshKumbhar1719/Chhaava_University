98acf2e7d961f3f572c1e8c9b2f0cc7a2c92d988
Nilesh Kumbhar <kumbharnilesh930@gmail.com>
2026-04-14 18:12:23 +0530

    Add project files.

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,32 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+// Load ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add Ocelot
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+await app.UseOcelot();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Departments/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Departments.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        
+
+        
+    }
+}
